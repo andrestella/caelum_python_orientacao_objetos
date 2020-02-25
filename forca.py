f0f86fddd6ecfef55acbc3f96747700cf3ddc7b0
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-25 19:51:07 -0300

    Alterando o Jogo da Forca

--- forca.py
+++ forca.py
@@ -2,35 +2,38 @@ print("*******************************")
 print("*        Jogo da Forca        *")
 print("*******************************")
 
-palavra_secreta = "banana"
-letras_acertadas = ["_","_","_","_","_","_"]
+def jogar():
+    palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
 
-acertou = False
-enforcou = False
-erros = 0
+    acertou = False
+    enforcou = False
+    erros = 0
 
-print(letras_acertadas)
-
-while not acertou and not enforcou:
-    chute = input("Qual a letra? ")
-    if chute in palavra_secreta:
-        posicao = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                print("Encontrei a letra {} na posição {}."
-                    .format(letra, posicao))
-                letras_acertadas[posicao] = letra
-            posicao += 1     
-    else:
-        erros += 1
-        print("Erro! Total de erros = {}".format(erros))
-    acertou = "_" not in letras_acertadas
-    enforcou = erros == 7
     print(letras_acertadas)
 
-if acertou:
-    print("Você ganhou!")
-else:
-    print("Você perdeu!")
+    while not acertou and not enforcou:
+        chute = input("Qual a letra? ")
+        if chute in palavra_secreta:
+            posicao = 0
+            for letra in palavra_secreta:
+                if chute.upper() == letra.upper():
+                    print("Encontrei a letra {} na posição {}."
+                        .format(letra, posicao))
+                    letras_acertadas[posicao] = letra
+                posicao += 1     
+        else:
+            erros += 1
+            print("Erro! Total de erros = {}".format(erros))
+        acertou = "_" not in letras_acertadas
+        enforcou = erros == 7
+        print(letras_acertadas)
 
-print("Fim do jogo!")
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
+
+    print("Fim do jogo!")
+
+jogar()
